6a76f7ce56f9db452671e356bfc89b8b13dce049
mprkatti <57388866+mprkatti@users.noreply.github.com>
2020-12-25 21:15:25 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   SITE_VERIFY_BASE_URL: `https://www.google.com/recaptcha/api/siteverify`,
-  SITE_SECRET: '6LdyxBEaAAAAANdtD7zGHqfSlERmTYKf2bk4pEVJ'
+  SITE_SECRET: 'skldjskdjsldskdlskdlskjl'
 };
 
 module.exports = config;
