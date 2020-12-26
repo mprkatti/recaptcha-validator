534f27b5df6b366fd0a5decbf2834f3d268af23a
mprkatti <praskatti@yahoo.com>
2020-12-26 17:14:02 +0530

    sending the score data back

--- src/index.js
+++ src/index.js
@@ -40,11 +40,13 @@ app.post('/feedback', (req, res) => {
 
   axios.post(VERIFY_URL).then((response) => {
     console.log('reCaptcha response: ', response.data);
+    res.status(200).json(response.data);
+
   }).catch((error) => {
     console.log('Inside error blk');
+    res.status(500).json({ "message": "internal error" });
   })
 
-  res.status(200).json('success');
 
 });
 
